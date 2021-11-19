bdd854d9b52239fa299f8dce512ea3c667d894b6
liaohang <liaohang@liaohangdeMacBook-Pro.local>
2021-11-19 11:58:20 +0800

    first commit

---
+++ flowwall/src/main/java/com/lt/flowwall/xPullRefresh/httpJsonHandle.java
@@ -0,0 +1,20 @@
+
+package com.lt.flowwall.xPullRefresh;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class httpJsonHandle {
+
+    private JSONObject returnJSONObject;
+    public JSONObject showToast(String json) throws JSONException {
+
+        returnJSONObject=new JSONObject(json) ;
+
+        return     returnJSONObject;
+
+    }
+
+
+
+}
